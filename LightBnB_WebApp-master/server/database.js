4015eb5f62d128f958923dee7e66a8d270e4d7de
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-30 19:12:35 +0000

    refactored getAllReservations function to interaction with a logged in user to see their reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -112,7 +112,30 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.*, properties.*
+
+    FROM reservations
+    
+    JOIN properties ON reservations.property_id = properties.id
+    
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    
+    WHERE reservations.guest_id = $1
+    
+    ORDER BY start_date
+    
+    LIMIT $2;
+
+    `, [guest_id, limit])
+    .then((result) => {
+      return Promise.resolve(result.rows);
+    })
+
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
